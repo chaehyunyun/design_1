593dd4d646f053ffea87f785f27dc426f92eaecd
chaehyunyun <79246447+chaehyunyun@users.noreply.github.com>
2021-04-26 13:41:12 +0900

    Initial commit

---
+++ app/src/main/java/androidtown/org/design_1/home.java
@@ -0,0 +1,58 @@
+package androidtown.org.design_1;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class home extends AppCompatActivity {
+    VideoView vv;
+    ImageView iv;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+
+        vv= findViewById(R.id.videoView2);
+        //Video Uri
+        Uri videoUri= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.mh);
+
+        //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
+        vv.setMediaController(new MediaController(this));
+
+        //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
+        vv.setVideoURI(videoUri);
+
+        //동영상을 읽어오는데 시간이 걸리므로..
+        //비디오 로딩 준비가 끝났을 때 실행하도록..
+        //리스너 설정
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //비디오 시작
+                vv.start();
+            }
+        });
+
+    }//onCreate ..
+
+    //화면에 안보일때...
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //비디오 일시 정지
+        if(vv!=null && vv.isPlaying()) vv.pause();
+    }
+    //액티비티가 메모리에서 사라질때..
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //
+        if(vv!=null) vv.stopPlayback();
+    }
+}
